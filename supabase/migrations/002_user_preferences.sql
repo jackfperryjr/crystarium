5e728d784e649bc357be83e37ef1e87210590224
Jack <jackfperryjr@gmail.com>
2026-05-16 21:15:15 -0500

    Updated migration for posterity.

--- supabase/migrations/002_user_preferences.sql
+++ supabase/migrations/002_user_preferences.sql
@@ -15,3 +15,5 @@ create policy "Users can manage own preferences"
   on public.user_preferences for all
   using  (auth.uid() = user_id)
   with check (auth.uid() = user_id);
+
+grant select, update on public.user_preferences to authenticated;
